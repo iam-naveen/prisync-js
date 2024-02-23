6f17a7ed09ce01ad95c3e5b4e6634b755e299b1c
Naveen <imnaveenbharath@gmail.com>
2024-02-24 01:59:42 +0530

    better logging and response handling

--- index.js
+++ index.js
@@ -21,18 +21,24 @@ app.get('/search', async (req, res) => {
           return res.send('Please provide a search keyword');
      }
 
+     const [amazonResponse, flipkartResponse] = await Promise.all([
+          fetch(`https://www.amazon.in/s?k=${keyword}`),
+          fetch(`https://www.flipkart.com/search?q=${keyword}`)
+     ]).catch(error => {
+          console.error('Error fetching data:', error);
+     });
+
+     console.log("got the data...");
+
+     const amazonContent = new JSDOM(await amazonResponse.text());
+     const flipkartContent = new JSDOM(await flipkartResponse.text());
+
+     console.log("parsing done...");
+
+     const data = [];
      try {
-          const [amazonResponse, flipkartResponse] = await Promise.all([
-               fetch(`https://www.amazon.in/s?k=${keyword}`),
-               fetch(`https://www.flipkart.com/search?q=${keyword}`)
-          ]);
-
-          const amazonContent = new JSDOM(await amazonResponse.text());
-          const flipkartContent = new JSDOM(await flipkartResponse.text());
-
-          const data = [];
           const amazonItems = amazonContent.window.document.body.querySelectorAll('[data-component-type="s-search-result"]')
-          for(let i = 0; i < 20; i++) {
+          for (let i = 0; i < 20; i++) {
                const title = amazonItems[i].querySelector('h2').textContent;
                const link = "https://amazon.in" + amazonItems[i].querySelector('a').href;
                const priceEle = amazonItems[i].querySelector('.a-price-whole');
@@ -42,8 +48,14 @@ app.get('/search', async (req, res) => {
                const rating = ratingEle ? ratingEle.textContent : 'No rating';
                data.push({ title, price, image, link, source: '/Amazon.png', rating });
           }
+          console.log("amazon done...");
+     } catch (error) {
+          console.log("error in amazon", error);
+     }
+
+     try {
           const flipkartItems = flipkartContent.window.document.body.querySelectorAll('._4ddWXP')
-          for(let i = 0; i < 20; i++) {
+          for (let i = 0; i < 20; i++) {
                const title = flipkartItems[i].querySelector('.s1Q9rs').textContent;
                const ratingEle = flipkartItems[i].querySelector('._3LWZlK');
                const rating = ratingEle ? ratingEle.textContent + " out of 5 stars" : 'No rating';
@@ -52,14 +64,14 @@ app.get('/search', async (req, res) => {
                const image = flipkartItems[i].querySelector('img').src;
                data.push({ title, price, image, link, source: '/flipkart.png', rating });
           }
-
-          res.render('items.html', { data });
+          console.log("flipkart done...");
      } catch (error) {
-          console.error('Error fetching data:', error);
-          res.status(500).send('Internal Server Error');
+          console.log("error in flipkart", error);
      }
-});
 
+     console.log("sending results...\n\n");
+     res.render('items.html', { data });
+});
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
      console.log(`Server is running on port ${port}`);
